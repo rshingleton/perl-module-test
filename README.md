ab8b2a0472845054f54f4b50c4cc5437451bf089
shingler <shingler@oclc.org>
2021-12-08 08:02:17 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+perl::module::test - It's new $module
+
+# SYNOPSIS
+
+    use perl::module::test;
+
+# DESCRIPTION
+
+perl::module::test is ...
+
+# LICENSE
+
+Copyright (C) shingler.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+shingler <shingler@oclc.org>
---
+++ lib/perl/module/test.pm
@@ -0,0 +1,39 @@
+package perl::module::test;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+perl::module::test - It's new $module
+
+=head1 SYNOPSIS
+
+    use perl::module::test;
+
+=head1 DESCRIPTION
+
+perl::module::test is ...
+
+=head1 LICENSE
+
+Copyright (C) shingler.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+shingler E<lt>shingler@oclc.orgE<gt>
+
+=cut
+
